bd0d87788921b9b02751d01ec1937fb9400541b1
Margulan Baimbet <mbaimbet@airba.kz>
2023-10-30 12:10:40 +0600

    wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,21 @@
 <x-layout>
-    <div class="container">
-        <div class="page">
-            <nav>
-                <div class="nav-logo">LOGO</div>
-                <div class="nav-links">
-                    <a href="https://google.com">Home</a>
-                    <a href="https://google.com">About</a>
-                    <a href="https://google.com">Works</a>
-                    <a href="https://google.com">Contact</a>
+    <div class="container page">
+        <nav>
+            <div class="nav-logo">LOGO</div>
+            <div class="nav-links">
+                <a href="https://google.com">Home</a>
+                <a href="https://google.com">About</a>
+                <a href="https://google.com">Works</a>
+                <a href="https://google.com">Contact</a>
 
-                    <button class="primary">Get in Touch</button>
-                </div>
-            </nav>
+                <button class="primary">Get in Touch</button>
+            </div>
+        </nav>
+
+        <div class="hero">
+            <h1>Элегантные <br> IT решения для вашего бизнеса без мишуры</h1>
+            <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, nisi odio qui rem reprehenderit
+                sapiente.</h2>
         </div>
     </div>
 </x-layout>
